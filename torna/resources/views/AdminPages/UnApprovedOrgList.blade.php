66bea27d7edb6c66c730920872dff07f01fc0d18
abhinixcel <abhijeet.bhosale@nixcelsoft.com>
2024-06-04 11:32:01 +0530

    Diary Changes Done till 04-06-2024 11:31pm

--- torna/resources/views/AdminPages/UnApprovedOrgList.blade.php
+++ torna/resources/views/AdminPages/UnApprovedOrgList.blade.php
@@ -313,7 +313,6 @@
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
         </style>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.1/dist/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
