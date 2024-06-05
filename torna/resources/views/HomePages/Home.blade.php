5daf7a7bde216e66ddf5b1e4ba6aaea9dce653b3
abhinixcel <abhijeet.bhosale@nixcelsoft.com>
2024-06-05 18:57:59 +0530

    Diary Changes Done till 05-06-2024 18:57pm

--- torna/resources/views/HomePages/Home.blade.php
+++ torna/resources/views/HomePages/Home.blade.php
@@ -33,7 +33,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
 </head>
 <style>
   .time-label {
@@ -114,7 +113,22 @@
     margin-bottom: 0; /* Remove bottom margin for copyright text */
     font-size: 15px; /* Adjust copyright text size */
 }
-       
+  /* Hide elements on mobile view */
+@media (max-width: 991.98px) {
+    .d-lg-none {
+        display: block !important;
+    }
+    .d-none {
+        display: none !important;
+    }
+}
+/* Hide elements on desktop view */
+@media (min-width: 992px) {
+    .d-lg-block {
+        display: block !important;
+    }
+}
+     
 </style>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light mb-3" id="ftco-navbar">
@@ -128,136 +142,31 @@
                     <li class="nav-item active"><a href="#" class="nav-link" style="color: white;">Home</a>                    </li>
                     <li class="nav-item"><a href="#exhibition" class="nav-link" style="color: white;">Exhibition</a></li>
                     <li class="nav-item"><a href="#contact" class="nav-link" style="color: white;">Contact</a></li>
-                    <li class="nav-item cta mr-md-2">
-                        <a href="#" class="nav-link register-btn" style="background-color: #FFBE07 !important; border-color: #FFBE07 !important; color: #fff !important;" data-toggle="popover" data-placement="bottom" 
-                        data-content='<a href="/registration" class="dropdown-item">As Organizer</a> <a href="/registrationE" class="dropdown-item">As Exhibitor</a>'>Register
-                        </a>
-                    </li>
-                    <li class="nav-item cta mr-md-2">
-                        <a href="/signin" class="nav-link" style="background-color: #FFBE07 !important; border-color: #FFBE07 !important; color: #fff !important;" >Sign In</a>
-                    </li>
+                    <!-- For desktop view -->
+<li class="nav-item cta mr-md-2 d-none d-lg-block">
+    <a href="#" class="nav-link register-btn" style="background-color: #FFBE07 !important; border-color: #FFBE07 !important; color: #fff !important;" data-toggle="popover" data-placement="bottom" data-content='<a href="/registration" class="dropdown-item">As Organizer</a> <a href="/registrationE" class="dropdown-item">As Exhibitor</a>'>Register</a>
+</li>
+<li class="nav-item cta mr-md-2 d-none d-lg-block">
+    <a href="/signin" class="nav-link" style="background-color: #FFBE07 !important; border-color: #FFBE07 !important; color: #fff !important;" >Sign In</a>
+</li>
+
+<!-- For mobile view -->
+<li class="nav-item cta mr-md-2 d-lg-none">
+    <a href="/registration" class="nav-link" style="background-color: #FFBE07 !important; border-color: #FFBE07 !important; color: #fff !important;"   data-content='<a href="/registration" class="dropdown-item">Register as Organizer</a>'>Register as Organizer</a>
+</li>
+<li class="nav-item cta mr-md-2 d-lg-none">
+    <a href="/registrationE" class="nav-link" style="background-color: #FFBE07 !important; border-color: #FFBE07 !important; color: #fff !important;"  data-content='<a href="/registrationE" class="dropdown-item">Register as Exhibitor</a>'>Register as Exhibitor</a>
+</li>
+<li class="nav-item cta mr-md-2 d-lg-none">
+    <a href="/signin" class="nav-link" style="background-color: #FFBE07 !important; border-color: #FFBE07 !important; color: #fff !important;">Sign In</a>
+</li>
+
                 </ul>
             </div>
         </div>
     </nav>
     <!-- END nav -->
 </br>
-
-    {{-- <div id="home" class="hero-wrap js-fullheight" style="background-image: url('images/imgs/bg_1.jpg');" data-stellar-background-ratio="0.5">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters align-items-center justify-content-start" style="margin-top: 170px;">
-          <div class="col-xl-10">
-            <h1 class="mb-4" style="color: #FF6D00; font-size: 60px;">Developer <br><span style="color: #FF6D00; font-weight: bold; font-size: 60px;">Exhibition 2024</span></h1>
-            <p id="event-date" class="mb-4" style="color: #FF6D00;"></p>
-            <div id="timer" class="d-flex mb-3">
-              <div class="time" id="dayss"></div>
-              <div class="time" id="hourss"></div>
-              <div class="time" id="minutess"></div>
-              <div class="time" id="secondss"></div>
-            </div>
-            <div class="d-flex">
-              <div class="time-label">Days</div>
-              <div class="time-label pl-4">Hours</div>
-              <div class="time-label pl-4">Minutes</div>
-              <div class="time-label pl-4">Seconds</div>
-            </div>
-          </div>
-        </div>
-       
-        
-      </div>
-    </div> --}}
-    {{-- <script>
-      // Set the target date to May 21, 2024
-      const targetDate = new Date('2024-06-28T00:00:00');
-      // Update the event date dynamically
-      const eventDateElement = document.getElementById('event-date');
-      const options = { year: 'numeric', month: 'long', day: 'numeric' };
-      const startDate = targetDate.toLocaleDateString('en-US', options);
-      const endDate = new Date(targetDate);
-      endDate.setDate(endDate.getDate() + 3);
-      const formattedEndDate = endDate.toLocaleDateString('en-US', options);
-      eventDateElement.innerHTML = `${startDate} - ${formattedEndDate}. Pimpri-Chinchwad, Pune`;
-    
-      function updateTimer() {
-        // Get the current date and time
-        const now = new Date();
-    
-        // Calculate the time difference in milliseconds
-        const diff = targetDate.getTime() - now.getTime();
-    
-        // Ensure the countdown doesn't show negative values
-        if (diff <= 0) {
-          document.getElementById('timer').innerHTML = '<div class="time">00</div><div class="time pl-4">00</div><div class="time pl-4">00</div><div class="time pl-4">00</div>';
-          return; // Stop the countdown
-        }
-    
-        // Convert milliseconds to days, hours, minutes, and seconds
-        const dayss = Math.floor(diff / (1000 * 60 * 60 * 24));
-        const hourss = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutess = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-        const secondss = Math.floor((diff % (1000 * 60)) / 1000);
-    
-        // Update the timer elements in the HTML
-        document.getElementById('dayss').innerText = padZero(dayss);
-        document.getElementById('hourss').innerText = padZero(hourss);
-        document.getElementById('minutess').innerText = padZero(minutess);
-        document.getElementById('secondss').innerText = padZero(secondss);
-      }
-    
-      // Helper function to pad zeros for single-digit numbers
-      function padZero(num) {
-        return num.toString().padStart(2, '0');
-      }
-    
-      // Update the timer every second
-      setInterval(updateTimer, 1000);
-    
-      // Initial call to update the timer immediately
-      updateTimer();
-    </script> --}}
-    {{-- <div class="row d-flex justify-content-center align-items-center">
-        <div class="col-lg-12">
-            <div class="carddd">
-                <div class="card-body">
-                    <h4 class="card-title text-center">ConneXha Exhibitions</h4>
-                    <div class="bootstrap-carousel">
-                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                @foreach($upcomingExs as $key => $upcomingEx)
-                                <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
-                                    <div class="row">
-                                        <div class="col-lg-12 col-md-7 col-sm-10 mb-2">
-                                            <div class="cardd">
-                                                @if($upcomingEx->company_logo)
-                                                <img src="data:image/png;base64,{{ $upcomingEx->company_logo }}" class="card-img-top" alt="Company Logo" style="width: 100%; height: 150px; object-fit: cover;">
-                                                @endif
-                                                <div class="card-body">
-                                                    <h5 class="card-title text-center" style="background-color: #FFBE07; padding: 0.2em 0.4em; border-radius: 4px; color: black;">
-                                                        {{ $upcomingEx->exhibition_name }}
-                                                    </h5>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                @endforeach
-                            </div>
-                            <a class="carousel-control-prev" href="#carouselExampleIndicators" data-slide="prev"><span class="carousel-control-prev-icon"></span> <span class="sr-only">Previous</span> </a><a class="carousel-control-next" href="#carouselExampleIndicators"
-                                data-slide="next"><span class="carousel-control-next-icon"></span> <span class="sr-only">Next</span></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> --}}
-    
 <div id="exhibition" class="content-body">
     <div class="container-fluid">
         <div class="row">
@@ -266,7 +175,7 @@
                 <br/>
                 <div class="row">
                     @foreach($upcomingExs as $key => $upcomingEx)
-                    <div class="col-lg-4 col-md-7 col-sm-10 mb-4">
+                    <div class="col-lg-4 col-md-7 col-sm-12 mb-4">
                         <div class="card">
                             @if($upcomingEx->company_logo)
                             <img src="data:image/png;base64,{{ $upcomingEx->company_logo }}" class="card-img-top" alt="Company Logo" style="width: 100%; height: 150px; object-fit: cover;">
@@ -457,11 +366,24 @@
     <script src="WebsiteAssets/js/main.js"></script>
 
     <script>
-        $(document).ready(function () {
+         $(document).ready(function () {
             $('[data-toggle="popover"]').popover({
-                html: true
+                html: true,
+                trigger: 'click'
+            }).on('show.bs.popover', function (e) {
+                e.stopPropagation();
+                $('.navbar-collapse').collapse('hide'); // Manually close the navbar
+            });
+
+            $(document).on('click', function (e) {
+                $('[data-toggle="popover"]').each(function () {
+                    if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+                        $(this).popover('hide');
+                    }
+                });
             });
         });
+
     </script>
 </body>
 </html>
